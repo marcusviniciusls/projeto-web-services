41e040ab4957329d2bb94dc06b4bac6330f0023f
Marcus Vinicius <marcus.silva@maps.com.br>
2021-09-23 20:59:02 -0300

    Projeto criado

---
+++ src/main/java/br/com/educandoweb/projetowebservices/ProjetoWebServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.educandoweb.projetowebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/educandoweb/projetowebservices/ProjetoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.educandoweb.projetowebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
